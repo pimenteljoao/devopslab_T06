9acf9f4b6b5d668e917e953876d024fcc6c28680
pimenteljoao <95723368+pimenteljoao@users.noreply.github.com>
2022-08-23 13:12:23 -0300

    acertos necessários

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
-csrf = CSRFProtect(app)                                                                                                                           
 
 @app.route("/")
 def pagina_inicial():
