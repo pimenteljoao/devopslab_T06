f9ddb0cae71b5e76f6fd408959bf100e66bbd728
pimenteljoao <95723368+pimenteljoao@users.noreply.github.com>
2022-08-23 12:12:51 -0300

    Fix-CSRF

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ class Test(unittest.TestCase):
         # compara o status da requisicao (precisa ser igual a 200)
         self.assertEqual(self.result.status_code, 200)
 
+   
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
         self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps")
