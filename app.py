d5532e7b3d6d868916548b98d33cf6f50b6ad93a
pimenteljoao <95723368+pimenteljoao@users.noreply.github.com>
2022-08-23 13:21:09 -0300

    Fix-CSRF

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def pagina_inicial():
+#     return "Alteração da mensagem original recuperei as aulas de sábado"
+
+# if __name__ == '__main__':
+#     app.run()
+
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app) 
+
 @app.route("/")
 def pagina_inicial():
-    return "Alteração da mensagem original recuperei as aulas de sábado"
+    return "Fix CSRF"
 
 if __name__ == '__main__':
     app.run()
--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Alteração da mensagem original recuperei as aulas de sábado")
+        self.assertEqual(self.result.data.decode('utf-8'), "Fix CSRF")
